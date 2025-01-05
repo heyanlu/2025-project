450f19b8ef7f0dedf3bb45751ca3e1620c99b2f7
QiuzhiLyon <zhiyueonline2023@gmail.com>
2025-01-05 10:00:29 -0800

    job2
    Class1 Spring boot 项目环境搭建

---
+++ src/test/java/com/qiuzhitech/onlineshopping_06/controller/HelloWorldControllerTest.java
@@ -0,0 +1,43 @@
+package com.qiuzhitech.onlineshopping_06.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+    @Resource
+    private HelloWorldController helloWorldController;
+
+    @Spy
+    FakeDependency dependency;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testAddPlug2() {
+        int result = helloWorldController.addPlug2(2,3);
+        assertEquals(7,result);
+    }
+
+    @Test
+    void testAddPlug2WithMock() {
+        helloWorldController = new HelloWorldController(dependency);
+        when(dependency.add(2,3))
+                .thenReturn(100);
+        int result = helloWorldController.addPlug2(2,3);
+        assertEquals(7, result);
+//  Write testable, secure application,
+//  keep 85% code coverage with unit test via Junit and  use Mockito for dependency mock.
+    }
+}
