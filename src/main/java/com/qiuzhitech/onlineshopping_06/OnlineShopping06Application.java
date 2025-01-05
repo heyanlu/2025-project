5bd0d7845841e153f39cf61ebfaf0f43cc5b971b
QiuzhiLyon <Peng@MacBook-Pro.local>
2025-01-05 09:22:55 -0800

    Init Spring Boot

---
+++ src/main/java/com/qiuzhitech/onlineshopping_06/OnlineShopping06Application.java
@@ -0,0 +1,13 @@
+package com.qiuzhitech.onlineshopping_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopping06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopping06Application.class, args);
+    }
+
+}
