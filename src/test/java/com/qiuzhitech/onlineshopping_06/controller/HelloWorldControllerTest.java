517293037d600030df2ae01e80594ddc0b151e27
QiuzhiLyon <zhiyueonline2023@gmail.com>
2025-01-13 21:28:48 -0800

    Chapter 2 了解Spring Boot 与MVC框架

--- src/test/java/com/qiuzhitech/onlineshopping_06/controller/HelloWorldControllerTest.java
+++ src/test/java/com/qiuzhitech/onlineshopping_06/controller/HelloWorldControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.annotation.Resource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -33,10 +34,10 @@ class HelloWorldControllerTest {
     @Test
     void testAddPlug2WithMock() {
         helloWorldController = new HelloWorldController(dependency);
-        when(dependency.add(2,3))
+        when(dependency.add(anyInt(),anyInt()))
                 .thenReturn(100);
         int result = helloWorldController.addPlug2(2,3);
-        assertEquals(7, result);
+        assertEquals(102, result);
 //  Write testable, secure application,
 //  keep 85% code coverage with unit test via Junit and  use Mockito for dependency mock.
     }
